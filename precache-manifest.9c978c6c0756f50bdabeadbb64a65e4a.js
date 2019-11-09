f3443cb2db8cec4f2aacec5b3d09b4c28b6247da
mor bargig <morbargig@gmail.com>
2019-11-09 03:48:26 +0200

    Updates

---
+++ precache-manifest.9c978c6c0756f50bdabeadbb64a65e4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa2babdb4e6c0f12ba586054811603d",
+    "url": "/Reflix/index.html"
+  },
+  {
+    "revision": "0568e745b04c930529a8",
+    "url": "/Reflix/static/css/main.3161690f.chunk.css"
+  },
+  {
+    "revision": "68bb9eef795e1e42abae",
+    "url": "/Reflix/static/js/2.582f0e3f.chunk.js"
+  },
+  {
+    "revision": "0568e745b04c930529a8",
+    "url": "/Reflix/static/js/main.ccd76603.chunk.js"
+  },
+  {
+    "revision": "c3bc20ee8027f25ebf87",
+    "url": "/Reflix/static/js/runtime~main.36913413.js"
+  }
+]);
